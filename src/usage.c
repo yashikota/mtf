9fd05fe28b93670a6996ea605c24daa2f3a581a4
kota <52403688+yashikota@users.noreply.github.com>
2023-08-12 01:01:27 +0900

    add usage

---
+++ src/usage.c
@@ -0,0 +1,11 @@
+#include "usage.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void usage(char *argv[]) {
+    fprintf(stderr, "Usage: %s [OPTION] FILE\n", argv[0]);
+    fprintf(stderr, "Option:\n");
+    fprintf(stderr, "  -o <FILE>  output file\n");
+    exit(EXIT_FAILURE);
+}
