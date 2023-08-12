59a4e110334e8578aa6b8c0ba9a555b897f50e12
kota <52403688+yashikota@users.noreply.github.com>
2023-08-12 11:43:04 +0900

    update list

--- src/list.c
+++ src/list.c
@@ -3,18 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void memoryError() {
-    fprintf(stderr, "Error: memory allocation failed.\n");
-    exit(EXIT_FAILURE);
-}
-
 List *createList(int size) {
     List *list = (List *)calloc(1, sizeof(List));
-    if (list == NULL) memoryError();
+    if (list == NULL) perror("createList");
     list->size = size;
     list->length = 0;
     list->data = (int *)calloc(size, sizeof(int));
-    if (list->data == NULL) memoryError();
+    if (list->data == NULL) perror("createList");
     return list;
 }
 
@@ -24,19 +19,26 @@ void resizeList(List *list, int size) {
         list->length = size;
     }
     temp = (int *)realloc(list->data, size * sizeof(int));
-    if (temp == NULL) memoryError();
+    if (temp == NULL) perror("resizeList");
     list->data = temp;
     list->size = size;
 }
 
-void pushList(List *list, int value) {
+int getListLength(List *list) { return list->length; }
+
+void cleanList(List *list) {
+    free(list->data);
+    free(list);
+}
+
+void pushValue(List *list, int value) {
     if (list->length == list->size) {
         resizeList(list, list->size * 2);
     }
     list->data[list->length++] = value;
 }
 
-int searchList(List *list, int value) {
+int searchValue(List *list, int value) {
     for (int i = 0; i < list->length; i++) {
         if (list->data[i] == value) {
             return i;
@@ -46,7 +48,7 @@ int searchList(List *list, int value) {
 }
 
 void deleteValue(List *list, int value) {
-    int index = searchList(list, value);
+    int index = searchValue(list, value);
     if (index == -1) {
         return;
     }
@@ -70,20 +72,4 @@ void insertValue(List *list, int index, int value) {
     list->length++;
 }
 
-int getListLength(List *list) { return list->length; }
-
-void cleanList(List *list) {
-    free(list->data);
-    free(list);
-}
-
-int getListValue(List *list, int index) { return list->data[index]; }
-
-int *getAllListValue(List *list) {
-    int *data = (int *)calloc(list->length, sizeof(int));
-    if (data == NULL) memoryError();
-    for (int i = 0; i < list->length; i++) {
-        data[i] = list->data[i];
-    }
-    return data;
-}
+int getValue(List *list, int index) { return list->data[index]; }
--- src/list.h
+++ src/list.h
@@ -8,13 +8,13 @@ typedef struct {
 } List;
 
 List *createList(int size);
-void pushList(List *list, int value);
-int searchList(List *list, int value);
+void pushValue(List *list, int value);
+int searchValue(List *list, int value);
 void deleteValue(List *list, int value);
 void insertValue(List *list, int index, int value);
 int getListLength(List *list);
 void cleanList(List *list);
-int getListValue(List *list, int index);
+int getValue(List *list, int index);
 int *getAllListValue(List *list);
 
 #endif
